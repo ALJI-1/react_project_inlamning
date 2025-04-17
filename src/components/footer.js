b71d6ebe279c8b522e4dcc2b62110ab8e67fe590
ALJI-1 <albin.ingelsson1@gmail.com>
2025-04-17 11:32:05 +0200

    .

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router';
 
 export  function Footer() {
+
+
   return (
     <div className="container px-4 py-4" id="footer">
         <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
 
-        <p className="col-md-4 mb-0 text-body-secondary">© 2024 Company, Inc</p>
-        <p className="col-md-4 mb-0 text-body-secondary">Creator: Snoop D.O. Double API<br />aka: Albin Ingelsson</p>
+        <p className="col-md-4 mb-0 text-muted">© 2024 Company, Inc</p>
+        <p className="col-md-4 mb-0 text-muted">Creator: Snoop D.O. Double API<br />aka: Albin Ingelsson</p>
 
         <ul className="nav col-md-4 justify-content-end"> 
-            <li className="nav-item"><a className="nav-link" href="ListPage.html">Music List</a></li>
-            <li className="nav-item"><a className="nav-link" href="index.html"  aria-current="page">About</a></li>
+
+            <li className="nav-item">
+            <Link style={{textDecoration: 'none'}} to="/artistList">
+                <button className="btn btn-link nav-link px-2 text-muted" >Music List</button>
+            </Link>
+            </li>
+            <li className="nav-item">
+            <Link style={{textDecoration: 'none'}} to="/about">
+                <button className="btn btn-link nav-link px-2 text-muted">About</button>
+            </Link>
+            </li>
+
         </ul>
 
         </footer>
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,21 +1,119 @@
 import React from 'react'
+import musicService from '../services/music-group-service';
+import { useEffect, useState } from 'react';
 
-export function HomePage() {
+export function HomePage(props) {
+  
+  const [wapiData, setWapiData] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const _service = new musicService(`https://seido-webservice-307d89e1f16a.azurewebsites.net/api`);
+      let data = await _service.readInfoAsync();
+      setWapiData(data); 
+    })();
+  }, []);
+  
+
   return (
-    <>
     <div className="container px-4 py-4" id="home">
-          <div className="bg-body-tertiary p-5 rounded">
-            <div className="col-sm-8 py-5 mx-auto">
-              <h1 className="display-5 fw-normal">Welcome to various templates</h1>
-                <p className="fs-5">This is a set of page templates using bootstrap giving example of
-                  of a modern adaptable UI for some common page needs. Simply use and modify the page 
-                  container you need.
+      <div className="bg-body-tertiary p-5 rounded text-center">
+        <div className="col-sm-8 py-5 mx-auto">
+          <h1 className="display-5 fw-bold">Your favorite music</h1>
+          <h2 className="pb-1" style={{ fontStyle: 'italic' }}>
+            Current available data, check it out!
+          </h2>
+          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5 g-4 py-5 justify-content-between">
+            <div className="col d-flex align-items-start">
+              <svg
+                className="bi text-body-secondary flex-shrink-0 me-3"
+                width="1.75em"
+                height="1.75em"
+              >
+                <use xlinkHref="#boombox" />
+              </svg>
+              <div>
+                <p className="bi bi-boombox mb-0 fs-1" style={{ alignContent: 'center' }}></p>
+                <h3 className="fw-bold mb-0 fs-4 text-body-emphasis">
+                  <a className="nav-link" href="ListPage.html">
+                    Music groups
+                  </a>
+                </h3>
+                <p
+                  id="count-groups"
+                  style={{
+                    color: 'rgb(255, 0, 0)',
+                    fontSize: '30px',
+                    fontWeight: 'bolder',
+                  }}
+                >
+                  
+                  {wapiData?.db.nrSeededMusicGroups}
+                
                 </p>
+              </div>
+            </div>
+            <div className="col d-flex align-items-start">
+              <svg
+                className="bi text-body-secondary flex-shrink-0 me-3"
+                width="1.75em"
+                height="1.75em"
+              >
+                <use xlinkHref="#album" />
+              </svg>
+              <div>
+                <p className="bi bi-vinyl mb-0 fs-1"></p>
+                <h3 className="fw-bold mb-0 fs-4 text-body-emphasis">Albums</h3>
+                <p
+                  id="count-albums"
+                  style={{
+                    color: 'rgb(255, 0, 0)',
+                    fontSize: '30px',
+                    fontWeight: 'bolder',
+                  }} >
+      
+                  {wapiData?.db?.nrSeededAlbums}
+                </p>
+              </div>
+            </div>
+            <div className="col d-flex align-items-start">
+              <svg
+                className="bi text-body-secondary flex-shrink-0 me-3"
+                width="1.75em"
+                height="1.75em"
+              >
+                <use xlinkHref="#artist" />
+              </svg>
+              <div>
+                <p className="bi bi-music-note-list mb-0 fs-1"></p>
+                <h3 className="fw-bold mb-0 fs-4 text-body-emphasis">Artists</h3>
+                <p
+                  id="count-artists"
+                  style={{
+                    color: 'rgb(255, 0, 0)',
+                    fontSize: '30px',
+                    fontWeight: 'bolder',
+                  }}
+                >
+                  {wapiData?.db?.nrSeededArtists}
               
-                <p>I also use favicon. Enjoy!!!!!</p>
+                </p>
+              </div>
             </div>
           </div>
+
+          <p
+            style={{
+              color: 'gold',
+              fontSize: 'xx-large',
+              fontWeight: 'bolder',
+              fontFamily: 'monospace',
+            }}
+          >
+            Enjoy!
+          </p>
         </div>
-    </>
-  )
+      </div>
+    </div>
+  );
 }
